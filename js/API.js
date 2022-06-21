4ac9394c90e9828f47516c8fc9f208a747d4bf8f
Jose Acosta <acostajoma9@gmail.com>
2022-06-21 08:07:05 -0600

    First commit: Added POST funcionality in the API file in order to add new clients to the CRM, also added a GET function to retrieve the clients on the DB. Included function to show the clients on the document and a function that displays an alert on the screen in case that there is an error

---
+++ js/API.js
@@ -0,0 +1,26 @@
+const url = 'http://localhost:4000/clientes';
+
+export const nuevocliente = async cliente => {
+    try {
+        await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(cliente),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error)
+    }
+}
